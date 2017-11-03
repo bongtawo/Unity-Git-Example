4d1a980c833ccfa251271f61bc6c3ee6fc8a93ec
BlueTree <bongtawo@naver.com>
2017-11-03 10:19:45 +0900

    Initial commit

---
+++ Assets/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
